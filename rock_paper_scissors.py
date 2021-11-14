2266bcec57efbd587be105f79095dacb2fc0bd9a
SebastianNeyra <neyrasebastian@gmail.com>
2021-11-14 14:59:08 -0500

    Added additional comments to rockpaperscissors file

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,4 +1,4 @@
-import random
+import random # importing a library
 
 def play(): # This is a function 
     user = input("What's your choice? 'r' for rock, 'p' for paper, 's' for scissors\n")
@@ -19,4 +19,4 @@ def is_win(player, opponent): # This is a function
      if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') or (player == 'p' and opponent == 'r'):
          return True
 
-print(play())
+print(play()) # calling the function
