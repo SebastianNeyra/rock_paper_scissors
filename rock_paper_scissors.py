abc07aaf643e9dac8dccbeb58a0d136d5d552c71
SebastianNeyra <neyrasebastian@gmail.com>
2021-11-14 14:54:05 -0500

    added rockpaperscissor file

---
+++ rock_paper_scissors.py
@@ -0,0 +1,22 @@
+import random
+
+def play(): # This is a function 
+    user = input("What's your choice? 'r' for rock, 'p' for paper, 's' for scissors\n")
+    computer = random.choice(['r', 'p', 's'])
+
+    if user == computer: 
+        return 'It\'s a tie'
+
+    # r > s, s > p, p > r
+    if is_win(user, computer): # This line will call is_win function below
+        return 'You won!' # If is_win returns True then we will get a 'You won' message
+
+    return 'You lost!' # If the function comes back as False then we will get a 'You lost!' message
+
+def is_win(player, opponent): # This is a function
+     # return true if player wins
+     # r > s, s > p, p > r
+     if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') or (player == 'p' and opponent == 'r'):
+         return True
+
+print(play())
